ebdfb969c29e09ecd863a6c7203429b1afd0e26c
Rowan Smith <theintheredwoods@gmail.com>
2019-05-08 16:08:58 -0500

    first draft of pet table

--- src/components/PetDashboard/PetDashboard.js
+++ src/components/PetDashboard/PetDashboard.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import './App.css';
+import PetTable from '../PetTable/PetTable'
 
 function PetDashboard() {
   return (
     <div >
       <h1> java app </h1>
+      <PetTable />
     </div>
+
   );
 }
 
